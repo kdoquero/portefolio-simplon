cd7f2adc7fb1d23dfe7ae7531169c14fda5356a1
kdoquero <kevindoquero@gmail.com>
2018-02-02 14:50:57 +0100

    inital commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="www/index.css">
+    <title>Portfolio of Kevin DOQUERO</title>
+</head>
+<body>
+    <div class="wallpaper">
+        <header id="header" class="container-row">      
+            <div>
+                <ul>
+                    <li><a href="https://github.com/kdoquero">GitHub</a></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+            <div class="container-column name">
+                <h1> Kévin DOQUERO</h1>
+                <h4>Web Developper Junior</h4>
+                <img src="" alt="">
+            </div>
+        </header>
+        <section>
+            
+
+        </section>
+        <section>
+
+        </section>
+        <footer>
+            
+        </footer>
+    </div>
+</body>
+</html>
